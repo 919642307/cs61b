db465bffbd5a9cfdd2442b487e70bd2d0094da34
919642307 <919642307@qq.com>
2021-05-27 15:52:22 +0800

    Update Board.java

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -5,11 +5,12 @@ import edu.princeton.cs.algs4.Queue;
 import javax.swing.border.TitledBorder;
 import java.util.PriorityQueue;
 
-public class Board implements WorldState{
+public class Board implements WorldState {
     private final int BLANK = 0;
-    private int [][] tiles;
-    private int n;
-    public Board(int [][] tile) {
+    private final int[][] tiles;
+    private final int n;
+
+    public Board(int[][] tile) {
         n = tile.length;
         tiles = new int[n][n];
         for (int i = 0; i < n; i++) {
@@ -18,15 +19,18 @@ public class Board implements WorldState{
             }
         }
     }
+
     public int tileAt(int i, int j) {
         if (i < 0 || i >= n || j < 0 || j >= n) {
             throw new IndexOutOfBoundsException();
         }
         return tiles[i][j];
     }
+
     public int size() {
         return n;
     }
+
     public Iterable<WorldState> neighbors() {
         Queue<WorldState> neighbors = new Queue<>();
         int hug = size();
@@ -60,6 +64,7 @@ public class Board implements WorldState{
         }
         return neighbors;
     }
+
     public int hamming() {
         int distance = 0;
         for (int i = 0; i < n; i++) {
@@ -67,13 +72,14 @@ public class Board implements WorldState{
                 if (tiles[i][j] == BLANK) {
                     continue;
                 }
-                if (tileAt(i , j) != properValue(i,j)) {
+                if (tileAt(i, j) != properValue(i, j)) {
                     distance++;
                 }
             }
         }
         return distance;
     }
+
     private int properValue(int i, int j) {
         return i * n + j + 1;
     }
@@ -85,31 +91,36 @@ public class Board implements WorldState{
                 if (tiles[i][j] == BLANK) {
                     continue;
                 }
-                if (tiles[i][j] != tileAt(i,j)) {
+                if (tiles[i][j] != properValue(i, j)) {
                     distance += getDifferent(tiles[i][j], i, j);
                 }
             }
         }
         return distance;
     }
+
     private int getDifferent(int s, int i, int j) {
         int[] index = intToXY(s);
-        return abs(index[0] , i) + abs(index[1], j);
+        return abs(index[0], i) + abs(index[1], j);
 
     }
+
     private int abs(int i, int j) {
         if (i > j) {
             return i - j;
         }
         return j - i;
     }
+
     private int[] intToXY(int s) {
-        return new int[] { (s - 1) / n, (s - 1) % n};
+        return new int[]{(s - 1) / n, (s - 1) % n};
     }
+
     @Override
-    public int estimatedDistanceToGoal(){
+    public int estimatedDistanceToGoal() {
         return manhattan();
     }
+
     public boolean equals(Object y) {
         if (this == y) {
             return true;
@@ -130,15 +141,18 @@ public class Board implements WorldState{
         }
         return true;
     }
-   /** Returns the string representation of the board.
-      * Uncomment this method. */
+
+    /**
+     * Returns the string representation of the board.
+     * Uncomment this method.
+     */
     public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tileAt(i,j)));
+                s.append(String.format("%2d ", tileAt(i, j)));
             }
             s.append("\n");
         }
